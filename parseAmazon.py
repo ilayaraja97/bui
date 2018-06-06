d4f06a52508ac2e63f22a191a58c57dfd61bf54d
himani <2015kucp1005@iiitkota.ac.in>
2018-06-06 17:18:31 +0530

    merged

--- parseAmazon.py
+++ parseAmazon.py
@@ -16,11 +16,36 @@ def parse_amazon():
     # 3 -> 0, 4 -> 1
     y_train = np.subtract(neg["overall"], 1)
     y_train = np.append(y_train, np.subtract(pos["overall"], 4))
-    # print(x_train)
-    # print(y_train)
+
+    # x_train = []
+    # y_train = []
+    # i = 0
+    # count1 = 0
+    # count5 = 0
+    # for x in data["overall"]:
+    #     if x == 5 and count5 < count1:
+    #         x_train.append(data["reviewText"][i])
+    #         y_train.append(1)
+    #         count5 += 1
+    #     if x == 1:
+    #         x_train.append(data["reviewText"][i])
+    #         y_train.append(0)
+    #         count1 += 1
+    #     i += 1
+    # x_train = np.copy(x_train).transpose()
+    # y_train = np.copy(y_train).transpose()
+    #
+    #
+    # # x_train = np.copy(data["reviewText"])
+    # # # 3 -> 0, 4 -> 1
+    # # y_train = np.round(np.divide(data["overall"], 7))
+    # # y_train = np.copy(data["overall"])
+    # # print(x_train)
+    # # print(y_train)
     # df = pd.DataFrame(y_train)
-    # print("h")
-    # print(np.divide(df["overall"].value_counts(), 1))  # 2786.77))
+    # # print(df.describe())
+    # #
+    # print(np.divide(df[0].value_counts(), 1))  # 2786.77))
     return x_train, y_train
 
 
